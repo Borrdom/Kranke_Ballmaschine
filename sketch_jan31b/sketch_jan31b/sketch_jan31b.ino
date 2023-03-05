29a752ba423f26be485e193e516b04f67f0bfb8e
Borrdom <86350432+Borrdom@users.noreply.github.com>
2023-03-05 18:49:05 +0100

    first_trial_shots

--- sketch_jan31b/sketch_jan31b/sketch_jan31b.ino
+++ sketch_jan31b/sketch_jan31b/sketch_jan31b.ino
@@ -1,47 +1,65 @@
 // Gleichstrommotoren
 
 //Motor 1
-int GSM1 = 10;  // PWM
-int in1 = 9;    // An aus Signal
-int in2 = 8;    // An aus Signal
+int GSM1 = 3;  // PWM
+int inr1 = 2;    // An aus Signal
+int inl1 = 4;    // An aus Signal
 //Motor 2
-int GSM2 = 6;  // PWM
-int in3 = 5;    // An aus Signal
-int in4 = 4;    // An aus Signal
+int GSM2 = 5;  // PWM
+int inr2 = 6;    // An aus Signal
+int inl2 = 7;    // An aus Signal
+//Motor 3
+int GSM3 = 10;  // PWM
+int inr3 = 9;    // An aus Signal
+int inl3 = 8;    // An aus Signal
 
-int a = 50;
-int b = 100;
+int threshold = 45;
 
-int Geschwindigkeit1 = 0;
+int Geschwindigkeit = 0;
+float scale= (255-threshold)/80;
 
 void setup()
 {
   Serial.begin(9600);  //serieller Monitor wird gestartet, Baudrate auf 9600 festgelegt
   //Motor 1
   pinMode(GSM1, OUTPUT);  
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  digitalWrite(in1, HIGH);  // Motor 1 beginnt zu rotieren
-  analogWrite(GSM1, 0);   // Motor 1 soll mit der Geschwindigkeit "200" (max. 255) rotieren
-  //Motor 2
+  pinMode(inr1, OUTPUT);
+  pinMode(inl1, OUTPUT);
+  digitalWrite(inr1, LOW); 
+  digitalWrite(inl1, HIGH); 
+  //Motor 1
   pinMode(GSM2, OUTPUT);  
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
-  digitalWrite(in3, HIGH);  // Motor 2 beginnt zu rotieren
-  analogWrite(GSM2, 0);   // Motor 2 soll mit der Geschwindigkeit "200" (max. 255) rotieren 
+  pinMode(inr2, OUTPUT);
+  pinMode(inl2, OUTPUT);
+  digitalWrite(inr2, LOW); 
+  digitalWrite(inl2, HIGH); 
+  //Motor 3
+  pinMode(GSM3, OUTPUT);  
+  pinMode(inr3, OUTPUT);
+  pinMode(inl3, OUTPUT);
+  digitalWrite(inr3, LOW); 
+  digitalWrite(inl3, HIGH); 
 }
 
 void loop(){
 
   if(Serial.available()) //wenn Daten empfangen werden...      
 {
-    Geschwindigkeit1=Serial.read();//..sollen diese ausgelesen werden#
-  if (Geschwindigkeit1<128){
-    analogWrite(GSM1, Geschwindigkeit1*2);   // Motor 1 soll mit der Geschwindigkeit "200" (max. 255) rotieren
+    Geschwindigkeit=Serial.read();//..sollen diese ausgelesen werden#
+  if (Geschwindigkeit<80){
+    analogWrite(GSM1, floor(Geschwindigkeit*scale)+threshold);   // Motor 1 soll mit der Geschwindigkeit "200" (max. 255) rotieren
 }
-  else{
-
-    analogWrite(GSM2, (Geschwindigkeit1-128)*2);   // Motor 1 soll mit der Geschwindigkeit "200" (max. 255) rotieren
+  else if (Geschwindigkeit>79 && Geschwindigkeit<160){
+    analogWrite(GSM2, floor((Geschwindigkeit-80)*scale)+threshold);   // Motor 1 soll mit der Geschwindigkeit "200" (max. 255) rotieren
+}
+  else if (Geschwindigkeit>159 && Geschwindigkeit<240){
+    analogWrite(GSM3, floor((Geschwindigkeit-160)*scale)+threshold);   // Motor 1 soll mit der Geschwindigkeit "200" (max. 255) rotieren
+}
+  else if (Geschwindigkeit>239 && Geschwindigkeit<256){
+    analogWrite(GSM1, 0);
+    analogWrite(GSM2, 0);
+    analogWrite(GSM3, 0);
+    //analogWrite(GSM3, (Geschwindigkeit-240)*3);   // Motor 1 soll mit der Geschwindigkeit "200" (max. 255) rotieren
 }
   }
 
