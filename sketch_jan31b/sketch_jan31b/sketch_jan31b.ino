25e8f404b10f7173ae32938a1ced10a2fbb7103f
Borrdom <86350432+Borrdom@users.noreply.github.com>
2023-03-11 21:02:21 +0100

    hello

--- sketch_jan31b/sketch_jan31b/sketch_jan31b.ino
+++ sketch_jan31b/sketch_jan31b/sketch_jan31b.ino
@@ -13,8 +13,18 @@ int GSM3 = 10;  // PWM
 int inr3 = 9;    // An aus Signal
 int inl3 = 8;    // An aus Signal
 
-int threshold = 45;
+float pi = 3.1415926535897932384626433832795;
+float x0 = 0.;
+float y0 = 0.01;
+float alpha0 = 0.;
+float alpha_tilde = 0.;
+float r_i = 0.;
+float w_A=0.;
+float w_B=0.;
+float w_C=0.;
+int P=0;
 
+int threshold = 0;
 int Geschwindigkeit = 0;
 float scale= (255-threshold)/80;
 
@@ -43,17 +53,23 @@ void setup()
 
 void loop(){
 
-  if(Serial.available()) //wenn Daten empfangen werden...      
-{
+//   if(Serial.available()) //wenn Daten empfangen werden...      
+// {
     Geschwindigkeit=Serial.read();//..sollen diese ausgelesen werden#
+
   if (Geschwindigkeit<80){
-    analogWrite(GSM1, floor(Geschwindigkeit*scale)+threshold);   // Motor 1 soll mit der Geschwindigkeit "200" (max. 255) rotieren
+    x0=Geschwindigkeit*scale+threshold;
+    x0=2/255*x0-1;
+    //analogWrite(GSM1, floor(x0));   // Motor 1 soll mit der Geschwindigkeit "200" (max. 255) rotieren
 }
   else if (Geschwindigkeit>79 && Geschwindigkeit<160){
-    analogWrite(GSM2, floor((Geschwindigkeit-80)*scale)+threshold);   // Motor 1 soll mit der Geschwindigkeit "200" (max. 255) rotieren
+    y0=(Geschwindigkeit-80)*scale+threshold;
+    y0=-(2/255*y0-1);
+    //analogWrite(GSM2, floor(y0));   // Motor 1 soll mit der Geschwindigkeit "200" (max. 255) rotieren
 }
   else if (Geschwindigkeit>159 && Geschwindigkeit<240){
-    analogWrite(GSM3, floor((Geschwindigkeit-160)*scale)+threshold);   // Motor 1 soll mit der Geschwindigkeit "200" (max. 255) rotieren
+    P=floor((Geschwindigkeit-160)*scale)+threshold;
+    //analogWrite(GSM3, P);   // Motor 1 soll mit der Geschwindigkeit "200" (max. 255) rotieren
 }
   else if (Geschwindigkeit>239 && Geschwindigkeit<256){
     analogWrite(GSM1, 0);
@@ -61,7 +77,64 @@ void loop(){
     analogWrite(GSM3, 0);
     //analogWrite(GSM3, (Geschwindigkeit-240)*3);   // Motor 1 soll mit der Geschwindigkeit "200" (max. 255) rotieren
 }
-  }
+
+    // x0 = 0.01;
+    // y0 = 0.01;
+    // P=0;
+
+    alpha_tilde = atan(abs(x0) / abs(y0)) / pi * 180.; // in Dummy Winkel umrechnen [deg]
+    r_i = min(1,sqrt(x0*x0 + y0*y0)); // in Radius umrechnen
+
+
+    if (x0 >= 0 && y0 >= 0){ // Sektor 1
+
+      alpha0 = alpha_tilde; // in realen Winkel umrechnen (von y-Achse im Uhrzeigersinn) [deg]
+    }
+    else if( x0 >= 0 && y0 < 0){ // Sektor 2
+
+      alpha0 = 180 - alpha_tilde; // in realen Winkel umrechnen (von y-Achse im Uhrzeigersinn) [deg]
+    }
+    else if (x0 < 0 && y0 < 0){ // Sektor 3
+
+      alpha0 = alpha_tilde + 180.; // in realen Winkel umrechnen (von y-Achse im Uhrzeigersinn) [deg]
+    }
+    else {// Sektor 4
+
+      alpha0 = 360 - alpha_tilde; // in realen Winkel umrechnen (von y-Achse im Uhrzeigersinn) [deg]
+    }
+
+    if (alpha0 >= 0 && alpha0 < 120.){ // Zwischen Motor A und B
+
+      w_A = 0.5 + 0.5 * cos((1.5 * (alpha0 - 0.)) * pi / 180.); // Anteil Motor A bei r = r_max
+      w_B = 0.5 + 0.5 * cos((1.5 * (alpha0 - 120.)) * pi / 180.); // Anteil Motor B bei r = r_max
+      w_C =  max(0,1 - w_A - w_B); // Anteil Motor C bei r = r_max
+    }
+    else if (alpha0 >= 120 && alpha0 < 240){ // Zwischen Motor B und C
+
+      w_B = 0.5 + 0.5 * cos((1.5 * (alpha0 - 120.)) * pi / 180.); // Anteil Motor B bei r = r_max
+      w_C = 0.5 + 0.5 * cos((1.5 * (alpha0 - 240.)) * pi / 180.); // Anteil Motor C bei r = r_max
+      w_A = max(0,1 - w_B - w_C); // Anteil Motor A bei r = r_max
+    }
+    else{  // Zwischen Motor C und A
+
+      w_A = 0.5 + 0.5 * cos((1.5 * (alpha0 - 360.)) * pi / 180.); // Anteil Motor A bei r = r_max
+      w_C = 0.5 + 0.5 * cos((1.5 * (alpha0 - 240.)) * pi / 180.); // Anteil Motor C bei r = r_max
+      w_B =  max(0,1 - w_A - w_C); // Anteil Motor B bei r = r_max
+    }
+
+    w_A = w_A / (w_A + w_B + w_C);
+    w_B = w_B / (w_A + w_B + w_C);
+    w_C = w_C / (w_A + w_B + w_C);
+
+    w_A = (w_A - 0.333) * r_i + 0.333; // Anteil Motor A bei r = r_i
+    w_B = (w_B - 0.333) * r_i + 0.333; // Anteil Motor B bei r = r_i
+    w_C = (w_C - 0.333) * r_i + 0.333; // Anteil Motor C bei r = r_i
+    
+    // Pmax=max(w_A*P,w_B*P,w_C*P);
+    analogWrite(GSM1, floor(w_A*P));
+    analogWrite(GSM2, floor(w_B*P));
+    analogWrite(GSM3, floor(w_C*P));
+  // }
 
   //
 
