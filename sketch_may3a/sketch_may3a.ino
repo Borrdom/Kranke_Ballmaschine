40caa579ace75108b5b1d726d92ed4ab4276b026
Borrdom <86350432+Borrdom@users.noreply.github.com>
2023-07-01 17:18:26 +0200

    volatiles get speed ultra

--- sketch_may3a/sketch_may3a.ino
+++ sketch_may3a/sketch_may3a.ino
@@ -4,9 +4,9 @@ int GSMA = 9;  // PWM
 int hallPinA = 2; 
 //Motor B
 int GSMB = 6;
-int hallPinB = 11; 
+int hallPinB = 3; 
 //Motor C
-int GSMC = 3;  // PWM
+int GSMC = 11;  // PWM
 int hallPinC = 12; 
 
 // Spinfunktion
@@ -19,7 +19,7 @@ float w_max = 1.;
 float scalexy= 255./80.;
 
 //pwmfunktion
-int pwmthresh=45;
+int pwmthresh=100;
 
 float pwmA = pwmthresh;
 float pwmB = pwmthresh;
@@ -80,8 +80,29 @@ int pwmthreshB=pwmthresh;
 int pwmthreshC=pwmthresh;
 volatile int valA=0;
 
-volatile long umdrehungenZeit;
-unsigned long letzteUmdrehungZeit;
+
+const byte numReadings = 2;  // Number of samples for smoothing. The higher, the more smoothing, but it's going to
+volatile long LastTimeWeMeasuredA=-1;  // Stores the last time we measured a pulse so we can calculate the period.
+volatile long LastTimeWeMeasuredB=-1;  // Stores the last time we measured a pulse so we can calculate the period.
+volatile unsigned long PeriodBetweenPulsesA = 0;
+volatile unsigned long PeriodBetweenPulsesB = 0;
+
+volatile unsigned long PeriodAverageA = 1;//ZeroTimeout+1000;  // Stores the period between pulses in microseconds in total, if we are taking multiple pulses.
+volatile unsigned long PeriodAverageB = 1;//ZeroTimeout+1000;  // Stores the period between pulses in microseconds in total, if we are taking multiple pulses.
+
+
+volatile unsigned int PulseCounterA = 0;  // Counts the amount of pulse readings we took so we can average multiple pulses before calculating the period.
+volatile unsigned int PulseCounterB = 0;  // Counts the amount of pulse readings we took so we can average multiple pulses before calculating the period.
+
+volatile unsigned long PeriodSumA=0; // Stores the summation of all the periods to do the average.
+volatile unsigned long PeriodSumB=0; // Stores the summation of all the periods to do the average.
+
+volatile unsigned int AmountOfReadings = 4;
+
+///
+
+
+
 
 void setup()
 {
@@ -104,76 +125,83 @@ void setup()
   analogWrite(GSMA, pwmthresh);
   analogWrite(GSMB, pwmthresh);
   analogWrite(GSMC, pwmthresh);
+  attachInterrupt(digitalPinToInterrupt(hallPinA), Pulse_EventA, RISING);
+  attachInterrupt(digitalPinToInterrupt(hallPinB), Pulse_EventB, RISING);
   // spannung= get_voltage(spannung);
 
   // maxspeedA=motorkurve(mA,spannung);
   // maxspeedB=motorkurve(mB,spannung);
   // maxspeedC=motorkurve(mC,spannung);
-  attachInterrupt(digitalPinToInterrupt(hallPinA), PENIS, FALLING && RISING);
-
+  // attachInterrupt(digitalPinToInterrupt(hallPinA), Pulse_Event, RISING);
+  // attachInterrupt(digitalPinToInterrupt(hallPinB), Pulse_Event, RISING);
+  // delay(1000);  // We sometimes take several readings of the period to average. Since we don't have any readings
+  //               // stored we need a high enough value in micros() so if divided is not going to give negative values.
+  //               // The delay allows the micros() to be high enough for the first few cycles.
+  
+  
   scale=maxspeedA/80.;
   // pwmthresh=1.2/spannung*255;
-  delta = floor((255.-pwmthresh)/(nMess+1.));
+  // delta = floor((255.-pwmthresh)/(nMess+1.));
 
-  for (int i = pwmthresh+delta; i > 0; i-=1){
-    analogWrite(GSMA,i);
-    analogWrite(GSMB,i);
-    analogWrite(GSMC,i);
-    if (readyA==0){ if ( not when_on(hallPinA) ) { pwmthreshA=i; readyA=1;};}
-    if (readyB==0){ if ( not when_on(hallPinB) ) { pwmthreshB=i; readyB=1;};}
-    if (readyC==0){ if ( not when_on(hallPinC) ) { pwmthreshC=i; readyC=1;};}
-    if ((readyA + readyB + readyC)==3){break;}
+  // for (int i = pwmthresh+delta; i > 0; i-=1){
+  //   analogWrite(GSMA,i);
+  //   analogWrite(GSMB,i);
+  //   analogWrite(GSMC,i);
+  //   if (readyA==0){ if ( not when_on(hallPinA) ) { pwmthreshA=i; readyA=1;};}
+  //   if (readyB==0){ if ( not when_on(hallPinB) ) { pwmthreshB=i; readyB=1;};}
+  //   if (readyC==0){ if ( not when_on(hallPinC) ) { pwmthreshC=i; readyC=1;};}
+  //   if ((readyA + readyB + readyC)==3){break;}
     
-  }
-    Serial.println(pwmthreshA);
-    Serial.println(pwmthreshB);
-    Serial.println(pwmthreshC);
+  // }
+  //   Serial.println(pwmthreshA);
+  //   Serial.println(pwmthreshB);
+  //   Serial.println(pwmthreshC);
 
-  // pwmthreshA=24;
-  // pwmthreshB=31;
-  // pwmthreshC=23;
+  // // pwmthreshA=24;
+  // // pwmthreshB=31;
+  // // pwmthreshC=23;
 
-    analogWrite(GSMA,255);
-    analogWrite(GSMB,255);
-    analogWrite(GSMC,255);
-    delay(2000);
-    maxspeedA=get_speed(hallPinA,speedA,450);
-    maxspeedB=get_speed(hallPinB,speedB,450);
-    maxspeedC=get_speed(hallPinC,speedC,450);
+  //   analogWrite(GSMA,255);
+  //   analogWrite(GSMB,255);
+  //   analogWrite(GSMC,255);
+  //   delay(2000);
+  //   maxspeedA=get_speed(hallPinA,speedA,450);
+  //   maxspeedB=get_speed(hallPinB,speedB,450);
+  //   maxspeedC=get_speed(hallPinC,speedC,450);
 
-    Serial.println("+++");
-    Serial.println(maxspeedA);
-    Serial.println(maxspeedB);
-    Serial.println(maxspeedC);
-    Serial.println("+++");
+  //   Serial.println("+++");
+  //   Serial.println(maxspeedA);
+  //   Serial.println(maxspeedB);
+  //   Serial.println(maxspeedC);
+  //   Serial.println("+++");
 
-  for (int i = 1; i < nMess+1; i+=1){
-    pwmtest=floor(255-delta*i);
-    Serial.println(pwmtest);
-    analogWrite(GSMA,pwmtest);
-    analogWrite(GSMB,pwmtest);
-    analogWrite(GSMC,pwmtest);
-    delay(2000);
-    speedA=get_speed(hallPinA,speedA,evaltest);
-    speedB=get_speed(hallPinB,speedB,evaltest);
-    speedC=get_speed(hallPinC,speedC,evaltest);
-    while (speedA>maxspeedA){speedA=get_speed(hallPinA,speedA,evaltest);}
-    while (speedB>maxspeedB){speedB=get_speed(hallPinB,speedB,evaltest);}
-    while (speedC>maxspeedC){speedC=get_speed(hallPinC,speedC,evaltest);}
+  // for (int i = 1; i < nMess+1; i+=1){
+  //   pwmtest=floor(255-delta*i);
+  //   Serial.println(pwmtest);
+  //   analogWrite(GSMA,pwmtest);
+  //   analogWrite(GSMB,pwmtest);
+  //   analogWrite(GSMC,pwmtest);
+  //   delay(2000);
+  //   speedA=get_speed(hallPinA,speedA,evaltest);
+  //   speedB=get_speed(hallPinB,speedB,evaltest);
+  //   speedC=get_speed(hallPinC,speedC,evaltest);
+  //   while (speedA>maxspeedA){speedA=get_speed(hallPinA,speedA,evaltest);}
+  //   while (speedB>maxspeedB){speedB=get_speed(hallPinB,speedB,evaltest);}
+  //   while (speedC>maxspeedC){speedC=get_speed(hallPinC,speedC,evaltest);}
 
-    // Serial.println(-(pwmtest-pwmthreshA)/log(1-speedA/maxspeedA));
-    // Serial.println(-(pwmtest-pwmthreshB)/log(1-speedB/maxspeedB));
-    // Serial.println(-(pwmtest-pwmthreshC)/log(1-speedC/maxspeedC));
+  //   // Serial.println(-(pwmtest-pwmthreshA)/log(1-speedA/maxspeedA));
+  //   // Serial.println(-(pwmtest-pwmthreshB)/log(1-speedB/maxspeedB));
+  //   // Serial.println(-(pwmtest-pwmthreshC)/log(1-speedC/maxspeedC));
 
-    pwmparamA=(pwmparamA*(float(i-1))-(pwmtest-pwmthreshA)/log(1-speedA/maxspeedA))/float(i);
-    pwmparamB=(pwmparamB*(float(i-1))-(pwmtest-pwmthreshB)/log(1-speedB/maxspeedB))/float(i);
-    pwmparamC=(pwmparamC*(float(i-1))-(pwmtest-pwmthreshC)/log(1-speedC/maxspeedC))/float(i);
+  //   pwmparamA=(pwmparamA*(float(i-1))-(pwmtest-pwmthreshA)/log(1-speedA/maxspeedA))/float(i);
+  //   pwmparamB=(pwmparamB*(float(i-1))-(pwmtest-pwmthreshB)/log(1-speedB/maxspeedB))/float(i);
+  //   pwmparamC=(pwmparamC*(float(i-1))-(pwmtest-pwmthreshC)/log(1-speedC/maxspeedC))/float(i);
 
 
-    Serial.println(pwmparamA);
-    Serial.println(pwmparamB);
-    Serial.println(pwmparamC);
-  }
+  //   Serial.println(pwmparamA);
+  //   Serial.println(pwmparamB);
+  //   Serial.println(pwmparamC);
+  // }
 
 
   
@@ -184,7 +212,8 @@ void loop(){
   Serial.println("---");
   //Serial.println(spannung);
   delay(1000);
-
+  speedA=get_speed_final(hallPinA);
+  speedB=get_speed_final(hallPinB);
 
   if (bluetooth){Signal=Serial.read();}//..sollen diese ausgelesen werden#
   
@@ -217,31 +246,38 @@ void loop(){
   // pwmparamA=speedregulation(speedA,sollspeedA,pwmparamA,KpA);
   
 
-  pwmA= pwm_func(sollspeedA, maxspeedA, pwmthreshA,pwmparamA);
-  analogWrite(GSMA, max(min(pwmA,255),pwmthreshA));
-  pwmB= pwm_func(sollspeedB, maxspeedB, pwmthreshB,pwmparamB);
-  analogWrite(GSMB, max(min(pwmB,255),pwmthreshB));
-  pwmC= pwm_func(sollspeedC, maxspeedC, pwmthreshC,pwmparamC);
-  analogWrite(GSMC, max(min(pwmC,255),pwmthreshC));
+  // speedA=get_speed_final(hallPinA);
 
-  delay(2000);
-  speedA=get_speed(hallPinA,speedA,eval);
-  Serial.println(abs(sollspeedA-speedA));
+
+  // attachInterrupt(digitalPinToInterrupt(hallPinA), Pulse_Event, RISING);
+  // speedA=get_speed_final();
+  // detachInterrupt(digitalPinToInterrupt(hallPinA));
+  // pwmA= pwm_func(sollspeedA, maxspeedA, pwmthreshA,pwmparamA);
+  // analogWrite(GSMA, max(min(pwmA,255),pwmthreshA));
+  // pwmB= pwm_func(sollspeedB, maxspeedB, pwmthreshB,pwmparamB);
+  // analogWrite(GSMB, max(min(pwmB,255),pwmthreshB));
+  // pwmC= pwm_func(sollspeedC, maxspeedC, pwmthreshC,pwmparamC);
+  // analogWrite(GSMC, max(min(pwmC,255),pwmthreshC));
+
+  // delay(2000);
+  // speedA=get_speed(hallPinA,speedA,eval);
+  // Serial.println(abs(sollspeedA-speedA));
   
 
-  // pwmparamB=speedregulation(speedB,sollspeedB,pwmparamB,KpB);
+  // // pwmparamB=speedregulation(speedB,sollspeedB,pwmparamB,KpB);
 
-  speedB=get_speed(hallPinB,speedB,eval);
-  Serial.println(abs(sollspeedB-speedB));
+  // speedB=get_speed(hallPinB,speedB,eval);
+  // Serial.println(speedB);
+  // Serial.println(abs(sollspeedB-speedB));
 
-  // pwmparamC=speedregulation(speedC,sollspeedC,pwmparamC,KpC);
+  // // pwmparamC=speedregulation(speedC,sollspeedC,pwmparamC,KpC);
 
-  speedC=get_speed(hallPinC,speedC,eval);
-  Serial.println(abs(sollspeedC-speedC));
+  // speedC=get_speed(hallPinC,speedC,eval);
+  // Serial.println(abs(sollspeedC-speedC));
 
-  sollspeedA= min(sollspeedA+100.,maxspeedA);
-  sollspeedB= min(sollspeedB+100.,maxspeedB);
-  sollspeedC= min(sollspeedC+100.,maxspeedC);
+  // sollspeedA= min(sollspeedA+100.,maxspeedA);
+  // sollspeedB= min(sollspeedB+100.,maxspeedB);
+  // sollspeedC= min(sollspeedC+100.,maxspeedC);
 
   
   // 
@@ -306,33 +342,7 @@ float get_speed(int hallPin, float speedold, int eval){
   return speed;
 }
 
-float get_speed_final(int hallPin, float speedold, int eval){
 
-
-  float speed = 0.;
-  // float start = micros();
-  umdrehungenZeit=micros();
-  letzteUmdrehungZeit=umdrehungenZeit;
-
-  //digitalWrite(LED_BUILTIN,HIGH);
-  valA=0;
-  while (valA<eval){
-  }
-  
-  // float stop = micros();
-  // float time=(stop-start)/1000000.;
-  float time=umdrehungenZeit/1000000.;
-  speed=valA/time*60;
-  return speed;
-}
-
-void PENIS(){
-  long now=micros();
-  valA++;
-  umdrehungenZeit+=(now-letzteUmdrehungZeit);
-  letzteUmdrehungZeit=now;
-  // Serial.println(val);
-}
 
 bool when_on(int hallPin){
   int val=0;
@@ -455,5 +465,89 @@ void xy_to_spin(float x0,float y0,float &w_A,float &w_B,float &w_C){
     w_C = (w_C - 1./3.) * r_i + 1./3.; // Anteil Motor C bei r = r_i
 }
 
+
+
+
+
+float get_speed_final(int hallPin){
+  float seconds=0;
+  if (hallPin==hallPinA){
+  noInterrupts();
+  seconds=PeriodAverageA/1000000.;
+  interrupts();
+  Serial.print("\tMotor A: ");
+  }
+  else if (hallPin==hallPinB){
+  noInterrupts();
+  seconds=PeriodAverageB/1000000.;
+  interrupts();
+  Serial.print("\tMotor B: ");
+  }
+
+  float rpm=60./seconds/2.;
+  Serial.print("\tRPM: ");
+  Serial.print(rpm);
+  Serial.print("\n");
+  return rpm;
+}
+
+ 
+void Pulse_EventA()  // The interrupt runs this to calculate the period between pulses:
+{
+  if (LastTimeWeMeasuredA==-1){
+  LastTimeWeMeasuredA=micros();
+  return;}
+
+  PeriodBetweenPulsesA = micros() - LastTimeWeMeasuredA;  // Current "micros" minus the old "micros" when the last pulse happens.
+                                                        // This will result with the period (microseconds) between both pulses.
+                                                        // The way is made, the overflow of the "micros" is not going to cause any issue.
+  LastTimeWeMeasuredA = micros();  // Stores the current micros so the next time we have a pulse we would have something to compare with.
+
+  // Serial.println(PeriodBetweenPulses);
+  if(PulseCounterA >= AmountOfReadings)  // If counter for amount of readings reach the set limit:
+  {
+    
+    PeriodAverageA = PeriodSumA / PulseCounterA;  // Calculate the final period dividing the sum of all readings by the
+                                                   // amount of readings to get the average.
+    PulseCounterA = 0;  // Reset the counter to start over. The reset value is 1 because its the minimum setting allowed (1 reading).
+    PeriodSumA = 0;  // Reset PeriodSum to start a new averaging operation.
+
+  }
+  else
+  {
+    PulseCounterA++;  // Increase the counter for amount of readings by 1.
+    PeriodSumA +=  PeriodBetweenPulsesA;  // Add the periods so later we can average.
+  }
+
+}  // End of Pulse_Event.
+
+void Pulse_EventB()  // The interrupt runs this to calculate the period between pulses:
+{
+  if (LastTimeWeMeasuredB==-1){
+  LastTimeWeMeasuredB=micros();
+  return;}
+
+  PeriodBetweenPulsesB = micros() - LastTimeWeMeasuredB;  // Current "micros" minus the old "micros" when the last pulse happens.
+                                                        // This will result with the period (microseconds) between both pulses.
+                                                        // The way is made, the overflow of the "micros" is not going to cause any issue.
+  LastTimeWeMeasuredB = micros();  // Stores the current micros so the next time we have a pulse we would have something to compare with.
+
+  // Serial.println(PeriodBetweenPulses);
+  if(PulseCounterB >= AmountOfReadings)  // If counter for amount of readings reach the set limit:
+  {
+    
+    PeriodAverageB = PeriodSumB / PulseCounterB;  // Calculate the final period dividing the sum of all readings by the
+                                                   // amount of readings to get the average.
+    PulseCounterB = 0;  // Reset the counter to start over. The reset value is 1 because its the minimum setting allowed (1 reading).
+    PeriodSumB = 0;  // Reset PeriodSum to start a new averaging operation.
+
+  }
+  else
+  {
+    PulseCounterB++;  // Increase the counter for amount of readings by 1.
+    PeriodSumB +=  PeriodBetweenPulsesB;  // Add the periods so later we can average.
+  }
+
+}  // End of Pulse_Event.
 
 
