1ad994b7146ba2493ac2ce03c0fc72e5ff8abbc6
Borrdom <86350432+Borrdom@users.noreply.github.com>
2024-08-10 20:42:35 +0200

    Add files via upload

---
+++ DHT22.ino
@@ -0,0 +1,26 @@
+#include "DHT.h" //DHT Bibliothek laden
+
+#define DHTPIN 10 //Der Sensor wird an PIN 2 angeschlossen    
+
+#define DHTTYPE DHT22    // Es handelt sich um den DHT22 Sensor
+
+DHT dht(DHTPIN, DHTTYPE); //Der Sensor wird ab jetzt mit „dth“ angesprochen
+
+void setup() {
+  Serial.begin(9600); //Serielle Verbindung starten
+
+  dht.begin(); //DHT22 Sensor starten
+}
+
+void loop() {
+  
+  delay(2000);//Zwei Sekunden bis zur Messung warten damit der Sensor etwas //messen kann weil er relativ langsam ist
+
+  
+  float Luftfeuchtigkeit = dht.readHumidity(); //die Luftfeuchtigkeit auslesen und unter „Luftfeutchtigkeit“ speichern
+  
+  float Temperatur = dht.readTemperature();//die Temperatur auslesen und unter „Temperatur“ speichern
+
+  Serial.println(Luftfeuchtigkeit); //die Dazugehörigen Werte anzeigen
+
+}
