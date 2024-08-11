9caad852acb8334e5dba8103e35818e60117a90a
Borrmann <dominik.borrmann@tu-dortmund.de>
2024-08-11 13:45:19 +0200

    added Hx711 sketch

--- testhumidifier/testhumidifier.ino
+++ testhumidifier/testhumidifier.ino
@@ -1,5 +1,5 @@
 
-int out = 9;
+int out = 10;
 void setup() {
   pinMode(out, OUTPUT);
   Serial.begin(9600);
@@ -12,10 +12,23 @@ void loop() {
     // delay(1000);
     // digitalWrite(out,175);
     // delay(1000);
-    digitalWrite(out,HIGH);
-    delay(1000);
+    analogWrite(out,255);
+    delay(100);
     
-    digitalWrite(out,LOW);
-    delay(5000);
+    analogWrite(out,0);
+    delay(100);
+    analogWrite(out,255);
+    delay(100);
+    analogWrite(out,235);
+    delay(1000);
+    digitalWrite(out,215);
+    delay(1000);
+    analogWrite(out,175);
+    delay(1000);
+    analogWrite(out,125);
+    delay(10000);
+    analogWrite(out,75);
+    delay(1000);
+    delay(1000);
     // delay(5000);
 }
